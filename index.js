57af95e5cc6620278e54641d253d52d647e64e9c
Nicholas Lysons <120535806+NicholasLysons@users.noreply.github.com>
2023-03-02 17:37:00 -0700

    Add files via upload
    laying down some background code

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require('inquirer');
+
+const generateHtml = ({ name, hobby }) =>     
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+questions = { name, hobby };
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
